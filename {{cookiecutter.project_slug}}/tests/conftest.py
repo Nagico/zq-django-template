1d6b0259c551227ebec507ac4407e1ac98c81dd4
Nagico <yjr888@vip.qq.com>
2023-04-04 14:45:49 +0800

    feat: 添加meilisearch

--- {{cookiecutter.project_slug}}/tests/conftest.py
+++ {{cookiecutter.project_slug}}/tests/conftest.py
@@ -27,6 +27,7 @@ def pytest_addoption(parser):
 
 def pytest_configure(config):
     os.environ["DJANGO_ENV"] = "test"
+    os.environ["DJANGO_SECRET_KEY"] = "secret"
     os.environ["DJANGO_SETTINGS_MODULE"] = "server.settings"
 
     os.environ["DATABASE_URL"] = "sqlite:///:memory:"
