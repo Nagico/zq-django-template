89c911f7c70b0a09a0879cffd2fe0e7337f309e0
Nagico <yjr888@vip.qq.com>
2023-08-26 16:21:19 +0800

    fix: 修复simpleui debug时的context异常

--- {{cookiecutter.project_slug}}/server/settings/components/common.py
+++ {{cookiecutter.project_slug}}/server/settings/components/common.py
@@ -30,6 +30,7 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
+                "server.utils.context_processors.custom_context",
             ],
         },
     },
